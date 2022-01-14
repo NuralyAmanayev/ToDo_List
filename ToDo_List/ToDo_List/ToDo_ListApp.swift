2fb245ae3a6f7c340424a5583accfffceaa771d2
NuralyAmanayev <92023725+NuralyAmanayev@users.noreply.github.com>
2022-01-15 02:54:15 +0300

    Add files via upload

---
+++ ToDo_List/ToDo_List/ToDo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_ListApp.swift
+//  ToDo_List
+//
+//  Created by Nuraly Amanayev on 9/24/21.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
